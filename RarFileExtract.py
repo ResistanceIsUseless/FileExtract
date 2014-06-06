25f262cce20379c6f5dd2e67c73b3dea0edaab03
mattgriff <matt.griffiths@gmail.com>
2014-06-06 13:42:09 -0700

    Update RarFileExtract.py

--- RarFileExtract.py
+++ RarFileExtract.py
@@ -1,24 +1,25 @@
 import glob,os,zipfile,shutil,rarfile
 
-dir = "/home/action/files"
-dest = "/tmp"
+dir = "/volume1/video/Movies/*/*/*"
+dest = "/volume1/video/extracted/"
 #dir = "C:\\Users\\user\\Downloads\\"
 #extractiondir = "C:\\Users\\user\\temp\\moved"
 
 rarfiles = glob.iglob(dir + "*.rar")
 
 for rfn in rarfiles:
-    source = os.path.abspath(os.path.realpath(rfn))
-    print("Extracting File", source)
-    z = rarfile.RarFile(source)
-    z.extractall()
-    currentdir = os.getcwd()
-    glob.glob(currentdir + ".r??")
+    path_with_file = os.path.abspath(os.path.realpath(rfn))
+    print("Extracting File", path_with_file)
+    rar = rarfile.RarFile(path_with_file)
+    cwd = os.path.dirname(path_with_file)
+    rar.extractall(cwd)
     print("Cleaning up..")
-    shutil.move(source,dest)
+    #all_rars = glob.glob(cwd + "*.r??")
+    shutil.move(cwd,dest)
 
 #except StopIteration:python
     #print("No more files found")
 
 else:
     print("No Files to extract")
+
